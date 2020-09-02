ec0e6abb27fa8ae1eab500aa876f2b9d73b648b7
U-Zo <kwoojo0127@naver.com>
2020-09-02 20:16:01 +0900

    Add scroll event for item list scroll fade-in, fade-out

--- introduce/js/main.js
+++ introduce/js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const items = document.querySelectorAll('.body-item');
+
+window.addEventListener('scroll', () => {
+  for (let i = 0; i < items.length; i++) {
+    if (i < items.length - 1) {
+      items[i].style.opacity = (items[i + 1].offsetTop - items[i].offsetTop) /
+          300;
+    }
+  }
+});
